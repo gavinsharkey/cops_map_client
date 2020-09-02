c523dda852a8f1a853aad112ced209d638393ef9
gavinsharkey <gavinsharkey2016@gmail.com>
2020-09-02 13:56:36 -0400

    Fetch and display markers on HomeScreen

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,7 +1,9 @@
 import React, { useState, useEffect } from 'react'
-import { View, Text, StyleSheet } from 'react-native'
-import MapView from 'react-native-maps'
+import { View, StyleSheet } from 'react-native'
+import MapView, { Marker } from 'react-native-maps'
 import * as Location from 'expo-location'
+import HomeScreenButtons from '../components/HomeScreenButtons'
+import HomeScreenMarker from '../components/HomeScreenMarker'
 
 const styles = StyleSheet.create({
   screen: {
@@ -9,23 +11,24 @@ const styles = StyleSheet.create({
     paddingBottom: '20%',
     alignItems: 'center',
     justifyContent: "flex-end"
-  },
-  searchBar: {
-    width: '90%',
-    backgroundColor: '#f2f2f7',
-    padding: 15,
-    borderRadius: 15,
-
   }
 })
 
-const HomeScreen = () => {
+const HomeScreen = ({ navigation }) => {
   const [region, setRegion] = useState({
     longitude: 0.0,
     latitude: 0.0,
     longitudeDelta: 0.1,
     latitudeDelta: 0.15
   })
+  
+  const [incidents, setIncidents] = useState([])
+
+  const fetchIncidents = () => {
+    fetch(`http://192.168.50.69:3000/incidents?latitude=${region.latitude}&longitude=${region.longitude}&radius=${region.longitudeDelta}`)
+    .then(resp => resp.json())
+    .then(json => setIncidents(json))
+  }
 
   useEffect(() => {
     Location.requestPermissionsAsync()
@@ -41,10 +44,12 @@ const HomeScreen = () => {
 
   return (
     <View style={styles.screen}>
-      <MapView onRegionChange={setRegion} region={region} style={{...StyleSheet.absoluteFill}} />
-      <View style={styles.searchBar}>
-        <Text>Bar</Text>
-      </View>
+      <MapView onRegionChange={setRegion} region={region} style={{...StyleSheet.absoluteFill}}>
+        {incidents.map(incident => (
+          <HomeScreenMarker navigation={navigation} key={incident.id} incident={incident} />
+        ))}
+      </MapView>
+      <HomeScreenButtons fetchIncidents={fetchIncidents} />
     </View>
   )
 }
