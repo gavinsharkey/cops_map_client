8509563ddb5aac9dcbb2769d9d135bf60c9a3962
gavinsharkey <gavinsharkey2016@gmail.com>
2020-09-02 19:34:52 -0400

    Add custom styling to HomeScreenMarker

--- components/HomeScreenMarker.js
+++ components/HomeScreenMarker.js
@@ -4,13 +4,29 @@ import { Marker, Callout } from 'react-native-maps'
 
 const HomeScreenMarker = ({ incident, navigation }) => {
   return (
-    <Marker coordinate={{latitude: incident.latitude, longitude: incident.longitude}}>
-      <Callout style={{flexDirection: 'row', alignItems: 'center'}} onPress={() => navigation.navigate('Incident', { id: incident.id })}>
-        <Text>{incident.title}</Text>
-        <Text style={{marginLeft: 15, opacity: 0.5}}>{'>'}</Text>
+    <Marker pinColor="#c51f5d" coordinate={{latitude: incident.latitude, longitude: incident.longitude}}>
+      <Callout tooltip onPress={() => navigation.navigate('Incident', { id: incident.id })}>
+        <View style={styles.tooltip}>
+          <Text style={styles.tooltipText}>{incident.title}</Text>
+          <Text style={{...styles.tooltipText, marginLeft: 10}}>{'>'}</Text>
+        </View>
       </Callout>
     </Marker>
   )
 }
+
+const styles = StyleSheet.create({
+  tooltip: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    padding: 15,
+    borderRadius: 15,
+    backgroundColor: '#243447'
+  },
+  tooltipText: {
+    color: '#fff',
+    fontWeight: '600'
+  }
+})
 
 export default HomeScreenMarker
