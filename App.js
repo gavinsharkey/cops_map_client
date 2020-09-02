0d667c166b514b2f51632f9319263334bcaea27a
gavinsharkey <gavinsharkey2016@gmail.com>
2020-09-02 19:32:21 -0400

    create redux store and provide it to app Add style for IncidentScreen header

--- App.js
+++ App.js
@@ -1,19 +1,33 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { createStore, applyMiddleware } from 'redux'
+import { Provider } from 'react-redux'
+import thunk from 'redux-thunk'
+import reducer from './reducers/rootReducer'
 import { NavigationContainer } from '@react-navigation/native'
 import { createStackNavigator } from '@react-navigation/stack'
 import HomeScreen from './screens/HomeScreen'
 import IncidentScreen from './screens/IncidentScreen';
 
+const store = createStore(reducer, applyMiddleware(thunk))
+
 const Stack = createStackNavigator()
 
 export default function App() {
   return (
-    <NavigationContainer initialRouteName="Home">
-      <Stack.Navigator>
-        <Stack.Screen name="Home" component={HomeScreen} options={{headerShown: false}} />
-        <Stack.Screen name="Incident" component={IncidentScreen} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <Provider store={store}>
+      <NavigationContainer initialRouteName="Home">
+        <Stack.Navigator>
+          <Stack.Screen name="Home" component={HomeScreen} options={{headerShown: false}} />
+          <Stack.Screen name="Incident" component={IncidentScreen} options={{
+            headerStyle: {
+              backgroundColor: '#243447'
+            },
+            headerTitleStyle: {
+              color: '#fff'
+            }
+          }} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </Provider>
   );
 }
