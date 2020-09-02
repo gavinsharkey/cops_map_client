98a98966458c656c0ee3ea04a13d6933142698e9
gavinsharkey <gavinsharkey2016@gmail.com>
2020-09-02 19:32:48 -0400

    Connect IncidentScreen to store add styles

--- screens/IncidentScreen.js
+++ screens/IncidentScreen.js
@@ -1,12 +1,34 @@
 import React, { Component } from 'react'
 import { View, Text, StyleSheet } from 'react-native'
+import { connect } from 'react-redux'
 
-export default class IncidentScreen extends Component {
+class IncidentScreen extends Component {
+  componentDidMount() {
+    this.props.navigation.setOptions({
+      title: this.props.incident.title
+    })
+  }
+
   render() {
     return (
-      <View>
-        <Text>incident id: {this.props.route.params.id}</Text>
+      <View style={styles.container}>
+        
       </View>
     )
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#141d26',
+  }
+})
+
+const mapStateToProps = (state, ownProps) => {
+  return {
+    incident: state.incidentsData.incidents.find(incident => incident.id == ownProps.route.params.id)
+  }
+}
+
+export default connect(mapStateToProps)(IncidentScreen)
