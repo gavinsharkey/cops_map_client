90f073e23469bcc09405739d8e0e5863fee87cfc
gavinsharkey <gavinsharkey2016@gmail.com>
2020-09-05 14:37:20 -0400

    Add BACKEND_URL constant

--- actions/incidentsActions.js
+++ actions/incidentsActions.js
@@ -1,7 +1,9 @@
+import BACKEND_URL from '../constants/BACKEND_URL'
+
 const fetchIncidents = (lat, long, radius) => {
   return dispatch => {
     dispatch({ type: 'LOADING_INCIDENTS' })
-    fetch(`http://192.168.50.69:3000/incidents?latitude=${lat}&longitude=${long}&radius=${radius}`)
+    fetch(`${BACKEND_URL}/incidents?latitude=${lat}&longitude=${long}&radius=${radius}`)
     .then(resp => resp.json())
     .then(json => dispatch({ type: 'SET_INCIDENTS', payload: json }))
   }
