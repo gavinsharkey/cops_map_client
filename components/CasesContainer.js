f1edb82db01bb39746dabe27a46f82a114867641
gavinsharkey <gavinsharkey2016@gmail.com>
2020-09-05 14:37:56 -0400

    Add CasesContainer

---
+++ components/CasesContainer.js
@@ -0,0 +1,30 @@
+import React, { useState, useEffect } from 'react'
+import { View, Text, FlatList, StyleSheet } from 'react-native'
+import BACKEND_URL from '../constants/BACKEND_URL'
+import CasesListItem from './CasesListItem'
+
+const CasesContainer = ({ id }) => {
+  const [loading, setLoading] = useState(true)
+  const [cases, setCases] = useState([])
+
+  useEffect(() => {
+    setLoading(true)
+    fetch(`${BACKEND_URL}/incidents/${id}/cases`)
+    .then(resp => resp.json())
+    .then(json => {
+      setCases(json)
+      setLoading(false)
+    })
+  }, [id])
+
+  return (
+    <View>
+      <FlatList refreshing={loading} keyExtractor={item => String(item.id)} data={cases} renderItem={({ item }) => (
+        <CasesListItem caseData={item} />
+      )} />
+    </View>
+  )
+}
+
+export default CasesContainer
+
