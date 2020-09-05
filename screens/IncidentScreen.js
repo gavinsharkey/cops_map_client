a4e34afba5184e502801307e51fb24de153d385a
gavinsharkey <gavinsharkey2016@gmail.com>
2020-09-05 14:37:46 -0400

    convert IncidentScreen to functional component

--- screens/IncidentScreen.js
+++ screens/IncidentScreen.js
@@ -1,27 +1,63 @@
-import React, { Component } from 'react'
+import React, { useEffect } from 'react'
 import { View, Text, StyleSheet } from 'react-native'
 import { connect } from 'react-redux'
+import CasesContainer from '../components/CasesContainer'
 
-class IncidentScreen extends Component {
-  componentDidMount() {
-    this.props.navigation.setOptions({
-      title: this.props.incident.title
+const IncidentScreen = ({ navigation, incident }) => {
+  useEffect(() => {
+    navigation.setOptions({
+      title: incident.title
     })
-  }
+  }, [])
 
-  render() {
-    return (
-      <View style={styles.container}>
-        
+  return (
+    <View style={styles.container}>
+      <View style={styles.textContainer}>
+        <Text style={styles.descriptionTitle}>Description:</Text>
+        <Text style={styles.description}>{incident.description}</Text>
       </View>
-    )
-  }
+      <View style={styles.casesContainer}>
+        <Text style={styles.casesTitle}>Cases</Text>
+        <CasesContainer id={incident.id} />
+      </View>
+    </View>
+  )
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    justifyContent: 'flex-start',
     backgroundColor: '#141d26',
+  },
+  textContainer: {
+    backgroundColor: '#fff',
+    padding: 15,
+    borderRadius: 15,
+    margin: 15
+  },
+  casesContainer: {
+    backgroundColor: '#fff',
+    borderTopStartRadius: 15,
+    borderTopEndRadius: 15,
+    paddingTop: 15
+  }, 
+  casesTitle: {
+    textAlign:'center',
+    fontSize: 20,
+    marginBottom: 15
+  },
+  descriptionTitle: {
+    fontWeight: '500',
+    fontSize: 30,
+    color: '#243447',
+    marginBottom: 5
+  },
+  description: {
+    fontWeight: '400',
+    fontSize: 30,
+    fontStyle: 'italic',
+    color: '#243447'
   }
 })
 
