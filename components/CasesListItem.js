a82b2bc4cabb08e8f00f0b60278ad5426714095b
gavinsharkey <gavinsharkey2016@gmail.com>
2020-09-06 09:18:46 -0400

    Change styles for CasesListItem

--- components/CasesListItem.js
+++ components/CasesListItem.js
@@ -5,7 +5,7 @@ import BACKEND_URL from '../constants/BACKEND_URL'
 const CasesListItem = props => {
   return (
     <View style={styles.container}>
-      <Image style={{width: 30, height: 30, flex: 1}} source={{ uri: `${BACKEND_URL}${props.caseData.media_url}` }} />
+      <Image style={styles.image} source={{ uri: `${BACKEND_URL}${props.caseData.media_url}` }} />
       <Text>Case {props.caseData.id}</Text>
     </View>
   )
@@ -15,8 +15,17 @@ const styles = StyleSheet.create({
   container: {
     borderColor: '#141d26',
     borderWidth: 1,
-    paddingVertical: 30,
-    flexDirection: 'row'
+    flexDirection: 'row',
+    justifyContent: 'flex-start',
+    alignItems: 'center',
+    borderRadius: 15,
+    overflow: 'hidden'
+  },
+  image: {
+    width: 100,
+    height: 100,
+    resizeMode: 'contain',
+    
   }
 })
 
