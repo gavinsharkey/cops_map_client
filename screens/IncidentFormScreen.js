d1ec42da3663732c83076e9409f9212510f802e0
gavinsharkey <gavinsharkey2016@gmail.com>
2020-09-13 09:56:15 -0400

    add incident form screen

---
+++ screens/IncidentFormScreen.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+import { NavigationContainer } from '@react-navigation/native'
+import { createStackNavigator } from '@react-navigation/stack'
+
+const IncidentFormScreen = () => {
+  return (
+    <View>
+      <Text></Text>
+    </View>
+  )
+}
