3aaca0e719ab9b43ceb65d6c6b25e6a431325895
gavinsharkey <gavinsharkey2016@gmail.com>
2020-09-13 09:44:12 -0400

    change what map tooltip displays

--- components/HomeScreenMarker.js
+++ components/HomeScreenMarker.js
@@ -7,7 +7,7 @@ const HomeScreenMarker = ({ incident, navigation }) => {
     <Marker pinColor="#c51f5d" coordinate={{latitude: incident.latitude, longitude: incident.longitude}}>
       <Callout tooltip onPress={() => navigation.navigate('Incident', { id: incident.id })}>
         <View style={styles.tooltip}>
-          <Text style={styles.tooltipText}>{incident.title}</Text>
+          <Text style={styles.tooltipText}>{incident.description}</Text>
           <Text style={{...styles.tooltipText, marginLeft: 10}}>{'>'}</Text>
         </View>
       </Callout>
