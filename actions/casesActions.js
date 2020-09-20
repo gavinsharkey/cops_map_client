029be6cfff07e00b6b836571391d828194204836
gavinsharkey <gavinsharkey2016@gmail.com>
2020-09-20 14:00:20 -0400

    Change endpoint to match route changes

--- actions/casesActions.js
+++ actions/casesActions.js
@@ -3,7 +3,7 @@ import BACKEND_URL from '../constants/BACKEND_URL'
 export const fetchCases = id => {
   return dispatch => {
     dispatch({type: 'LOADING_CASES'})
-    fetch(`${BACKEND_URL}/incidents/${id}/cases`)
+    fetch(`${BACKEND_URL}/cases?incident_id=${id}`)
     .then(resp => resp.json())
     .then(json => {
       dispatch({type: 'SET_CASES', payload: json})
